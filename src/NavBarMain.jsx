9c291021e6d3116dbbaf7f9e254d3dfdd4b26528
Hamza Elhamdadi <helhamdadi@umass.edu>
2025-01-21 10:52:40 -0500

    removed NavBar

--- src/NavBarMain.jsx
+++ src/NavBarMain.jsx
@@ -1,23 +1,16 @@
 import { NavLink } from 'react-router-dom';
 
-import Nav from 'react-bootstrap/Nav';
-import NavBar from 'react-bootstrap/NavBar';
-
 const NavBarMain = () => {
     return <>
       <p className='page-name'>CS 571 - Data Visualization and Exploration</p>
       <p className='page-name page-subname'>Spring 2025 - UMass Amherst </p>
-      <NavBar className='mx-auto' variant='light' sticky='top'>
-        <Nav className='ms-auto'>
+      <nav className="mx-auto navbar navbar-expand navbar-light sticky-top">
+        <div class="ms-auto navbar-nav">
           <NavLink className='nav-link page-name active' to='/'>Home</NavLink>
           <NavLink className='nav-link page-name' to="/syllabus">Syllabus</NavLink>
           <NavLink className='nav-link page-name' to="/schedule">Schedule</NavLink>
-        </Nav>
-        <Nav className='mr-auto'>
-          
-          
-        </Nav>
-      </NavBar>
+        </div>
+      </nav>
     </>
   };
 
